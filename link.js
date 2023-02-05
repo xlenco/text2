17d28191d77e05dd86161442bfe8893bbf72446b
Xlenco <75837117+xlenco@users.noreply.github.com>
2023-02-05 21:17:12 +0800

    Update link.js

--- link.js
+++ link.js
@@ -8,5 +8,9 @@ data.forEach((e, i) => {
     let j = data.length-1;  //获取友链数组的范围（除了最后一栏，前面的都获取）
     if (i != j) ls = ls.concat(e.link_list) 
 });
+if (!fs.existsSync('./dist')) {
+  fs.mkdirSync('./dist')
+}
+
 fs.writeFileSync('./dist/index.json', `{"link_list": ${JSON.stringify(ls)},"length":${ls.length}}`)
 console.log('友链文件已生成。');
